4ccfc380a2191cef44b9f6233f733d3076b03f21
Rahul Kumar Mishra <mishra.rahul1007@gmail.com>
2026-01-30 12:56:56 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          My First React App using GitHub
         </p>
         <a
           className="App-link"
